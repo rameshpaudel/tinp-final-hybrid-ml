c5a4530c1d1a0b97e993adee323d3a159d686ad0
Ramesh Paudel <rpaudel58@gmail.com>
2024-10-13 13:07:51 +1100

    Added counter to the scan history

--- models/scans.py
+++ models/scans.py
@@ -1,5 +1,6 @@
 import datetime
 from utils.main import BaseModel, db
+from sqlalchemy import func
 
 '''Displays the scan history of each files and their result'''
 class ScanHistory(BaseModel):
@@ -7,7 +8,7 @@ class ScanHistory(BaseModel):
     file_name = db.Column(db.String(150), index=True)
     hashed_name = db.Column(db.String(250), nullable=True)
     file_size = db.Column(db.String(15),nullable=True)
-    status = db.Column(db.String(15), nullable=True)
+    status = db.Column(db.String(25), nullable=True)
     results = db.Column(db.String(150), nullable=True)
     details = db.Column(db.JSON, nullable=True)
     request_info = db.Column(db.JSON, nullable=True)
@@ -30,3 +31,9 @@ class ScanHistory(BaseModel):
         "user_id",
         "created_at"
     ]
+    
+    #Get the total count of scan
+    def get_total_scans(user_id=None):   
+        if user_id:
+                return db.session.query(func.count(ScanHistory.id)).filter_by(user_id=user_id).scalar()
+        return db.session.query(func.count(ScanHistory.id)).scalar()
